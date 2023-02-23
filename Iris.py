6993220094362c8e35650e89e01366ad2b83239d
Edubuenu <107925516+Edubuenu@users.noreply.github.com>
2023-02-23 17:38:09 -0300

    Add files via upload

---
+++ Iris.py
@@ -0,0 +1,105 @@
+#Vamos importar o sklearn
+from sklearn import datasets
+#Agora vamos carregar a base de dados das íris de flores
+iris = datasets.load_iris()
+print(type(iris))
+#Dê uma primeira olhada nos dados
+#Cada linha é uma observação (também conhecida como: amostra, exemplo, instância, registro)
+#Cada coluna é um recurso (também conhecido como: feature, preditor, atributo, variável independente, entrada, regressor, covariável)
+print(iris.data)
+print('-'*40)
+#Imprima os nomes dos quatro recursos (features)
+print(iris.feature_names)
+#Liste as classes alvo da classificação
+print(list(iris.target_names))
+#O comando a seguir imprime inteiros representando as espécies de cada observação: 0, 1 e 2 representam espécies diferentes.
+print(iris.target)
+print('Type data')
+print(type(iris.data))
+print('Type target')
+print(type(iris.target))
+print('_-' * 20)
+#Verifique o formato das features ( primeira dimensão = número de observações, segunda dimensão = número de features )
+print(iris.data.shape)
+#numero de observacoes
+print(iris.target.shape)
+#Armazena a matriz de recurso (feature) em “x”
+x = iris.data
+#Armazena o vetor de resposta em “y”
+y = iris.target
+#Primeira dimensão = (LINHAS) ou seja, número de observações
+#Segunda dimensões = (COLUNAS) ou seja, número de features.
+print(x.shape)
+#Verifique a forma da resposta (dimensão única correspondente ao número de observação)
+print(y.shape)
+print(y.reshape(y.shape[0],-1))
+print(y.shape)
+print('Gráfico de dispersão com o conjunto de dados Iris')
+featuresAll=[]
+features = iris.data[: , [0,1,2,3]]
+print(features.shape)
+print(iris.feature_names)
+print('-' * 40)
+#Toda observação será anexada na lista assim que for lida.
+#Um loop for é usado para o processo de iteração.
+#Para cada linha lida, os valores das features são somados.
+for observation in features:
+    featuresAll.append([observation[0] + observation[1] + observation[2] + observation[3]])
+print(featuresAll)
+print('_'*40)
+import matplotlib.pyplot as plt
+plt.scatter(featuresAll, y, color='red', alpha =1.0)
+plt.rcParams['figure.figsize'] = [10,8]
+plt.title('Iris Dataset scatter Plot')
+plt.xlabel('Features')
+plt.ylabel('Targets')
+print('Gráfico')
+print(plt.show())
+print('fim do gráifco')
+print('#'*30)
+#Encontrando o relacionamento entre o comprimento e a largura da sépala
+sepal_len = []
+sepal_width = []
+for feature in features:
+    sepal_len.append(feature[0]) #Comprimento da sépala
+    sepal_width.append(feature[1]) #Largura da sépala
+
+groups = ('Iris-setosa','Iris-versicolor','Iris-virginica')
+colors = ('blue', 'green','red')
+data = ((sepal_len[:50], sepal_width[:50]), (sepal_len[50:100], sepal_width[50:100]), 
+        (sepal_len[100:150], sepal_width[100:150]))
+
+
+for item, color, group in zip(data, colors, groups): 
+    #item = (sepal_len[:50], sepal_width[:50]), (sepal_len[50:100], sepal_width[50:100]), 
+    #(sepal_len[100:150], sepal_width[100:150])
+    x0, y0 = item
+    plt.scatter(x0, y0,color=color,alpha=1)
+    plt.title('Iris Dataset scatter Plot')
+
+plt.xlabel('Sepal length')
+plt.ylabel('Sepal width')
+print(plt.show())
+print('-'*30)
+#Encontrando o relacionamento entre o comprimento e a largura da pétala
+petal_len = []
+petal_width = []
+for feature in features:
+    petal_len.append(feature[2]) #Comprimento da pétala
+    petal_width.append(feature[3]) #Largura da pétala
+groups = ('Iris-setosa','Iris-versicolor','Iris-virginica')
+colors = ('blue', 'green','red')
+data = ((petal_len[:50], petal_width[:50]), (petal_len[50:100], petal_width[50:100]), 
+        (petal_len[100:150], petal_width[100:150]))
+
+for item, color, group in zip(data,colors,groups): 
+    #item = (petal_len[:50], petal_width[:50]), (petal_len[50:100], petal_width[50:100]), 
+    #(petal_len[100:150], petal_width[100:150])
+    x0, y0 = item
+    plt.scatter(x0, y0,color=color,alpha=1)
+    plt.title('Iris Dataset scatter Plot')
+
+plt.xlabel('Petal length')
+plt.ylabel('Petal width')
+print(plt.show())
+
---
+++ Pandas_as_pd.py
@@ -0,0 +1,172 @@
+#importa a biblioteca pandas como pd
+import pandas as pd
+#importa o numpy como np
+import numpy as np
+
+#Cria um array numpy
+data = np.array(['a','b','c','d'])
+
+#Usa o array numpy criado acima para 
+#gerar um objeto Series do pandas
+s1 = pd.Series(data)
+
+print(s1)
+
+#Cria um array numpy
+data2 = np.array(['a','b','c','d'])
+s2 = pd.Series(data2, index = [70, 71, 72, 73])
+print(s2)
+
+data3 = {'a' : 0., 'b' : 1., 'c' : 2.}
+s3 = pd.Series(data3)
+print(s3)
+
+data4 = {'a' : 0., 'b' : 1., 'c' : 2.}
+s4 = pd.Series(data4, index = ['b','c','d','a'])
+print(s4)
+
+print('-'*30)
+
+s = pd.Series([1, 2, 3, 4, 5], index = ['a','b','c','d','e'])
+
+#recupera um único elemento
+print(s['a'])
+
+#recupera vários elementos
+print(s[['a','c','d']])
+
+#recupera vários elementos
+print(s['e']) #chave que n existe dá erro
+
+print('*'*30)
+#Vamos criar um DataFrame vazio
+print('Vamos criar um DataFrame vazio')
+df = pd.DataFrame()
+print(df)
+print()
+data = [1, 2, 3, 4, 5]
+df = pd.DataFrame(data)
+print(df)
+
+print()
+
+data = [ ['Maria', 10], ['Carlos', 12], ['Paulo', 13] ]
+df = pd.DataFrame(data, columns = ['Nome', 'Idade'])
+print(df)
+
+df = pd.DataFrame(data, columns = ['Nome', 'Idade'], dtype = float)
+print(df)
+
+data = {'Nome':['Marcos', 'Paula', 'Lia', 'Carlos'], 'Idade':[28, 34, 29, 42]}
+df = pd.DataFrame(data)
+print(df)
+
+df = pd.DataFrame(data, index = ['rank1', 'rank2', 'rank3', 'rank4'])
+print(df)
+
+print('*'*30)
+
+data = [ {'a': 1, 'b': 2}, {'a': 5, 'b': 10, 'c': 20} ]
+df = pd.DataFrame(data)
+print(df)
+print()
+df = pd.DataFrame(data, index = ['primeiro', 'segundo'])
+print(df)
+print()
+data = [ {'a': 1, 'b': 2}, {'a': 5, 'b': 10, 'c': 20} ]
+
+#Com dois índices de colunas, os valores são iguais aos das chaves do dicionário
+df1 = pd.DataFrame(data, index = ['primeiro', 'segundo'], columns = ['a', 'b'])
+
+#Com dois índices de colunas com um índice com outro nome
+df2 = pd.DataFrame(data, index = ['primeiro', 'segundo'], columns = ['a', 'b1'])
+print(df1)
+print(df2)
+
+print('Series')
+print('*'*30)
+
+dic = {'um' : pd.Series([1, 2, 3], index = ['a', 'b', 'c']),
+   'dois' : pd.Series([1, 2, 3, 4], index = ['a', 'b', 'c', 'd'])}
+
+df = pd.DataFrame(dic)
+print(df)
+
+dic = {'um' : pd.Series([1, 2, 3], index = ['a', 'b', 'c']),
+   'dois' : pd.Series([1, 2, 3, 4], index = ['a', 'b', 'c', 'd'])}
+
+df = pd.DataFrame(dic)
+print(df ['um'])
+
+print()
+
+
+dic = {'um' : pd.Series([1, 2, 3], index = ['a', 'b', 'c']),
+   'dois' : pd.Series([1, 2, 3, 4], index = ['a', 'b', 'c', 'd'])}
+
+df = pd.DataFrame(dic)
+
+# Adicionando uma nova coluna a um objeto DataFrame existente 
+# com rótulo de coluna passando nova Series
+
+print ("Adicionando uma nova coluna passando como Série:")
+df['três'] = pd.Series([10, 20, 30], index = ['a','b','c'])
+print(df)
+
+print ("Adicionando uma nova coluna usando as colunas existentes no DataFrame")
+df['quatro'] = df['um'] + df['três']
+
+print(df)
+
+print()
+
+print('-'*30)
+
+d = {'um' : pd.Series([1, 2, 3], index = ['a', 'b', 'c']), 
+   'dois' : pd.Series([1, 2, 3, 4], index = ['a', 'b', 'c', 'd']), 
+   'três' : pd.Series([10,20,30], index = ['a','b','c'])}
+
+df = pd.DataFrame(d)
+print ("Nosso dataframe é:")
+print(df)
+
+# usando a função del
+print ("Excluindo a primeira coluna usando a função DEL:")
+del df['um']
+print(df)
+
+# usando a função pop
+print ("Excluindo outra coluna usando a função POP:")
+df.pop('dois')
+print(df)
+
+dic = {'um' : pd.Series([1, 2, 3], index = ['a', 'b', 'c']), 
+   'dois' : pd.Series([1, 2, 3, 4], index = ['a', 'b', 'c', 'd'])}
+
+df = pd.DataFrame(dic)
+print(df.loc['b'])
+
+
+dic = {'um' : pd.Series([1, 2, 3], index = ['a', 'b', 'c']),
+   'dois' : pd.Series([1, 2, 3, 4], index = ['a', 'b', 'c', 'd'])}
+
+df = pd.DataFrame(dic)
+print(df.iloc[2])
+
+print()
+df = pd.DataFrame([[1, 2], [3, 4]], columns = ['a','b'])
+df2 = pd.DataFrame([[5, 6], [7, 8]], columns = ['a','b'])
+
+df = df.append(df2)
+print(df)
+
+df = pd.DataFrame([[1, 2], [3, 4]], columns = ['a','b'])
+df2 = pd.DataFrame([[5, 6], [7, 8]], columns = ['a','b'])
+
+df = df.append(df2)
+
+#Deleta as linhas com o rótulo 0
+df = df.drop(0)
+
+print(df)
+
